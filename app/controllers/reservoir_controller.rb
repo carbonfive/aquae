816c10bac3c3db10114635e3f1218b4e74a22433
George Masgras <gmasgras@gmail.com>
2014-10-21 16:13:34 -0700

    add more Reservoir data to JSON response

--- app/controllers/reservoir_controller.rb
+++ app/controllers/reservoir_controller.rb
@@ -4,7 +4,8 @@ class ReservoirController < ApplicationController
   def index
     factory = RGeo::GeoJSON::EntityFactory.instance
     features = Reservoir.all.map { |res|
-      factory.feature res.latlon,nil, { desc: { name: res.name} }
+      factory.feature res.latlon,nil, { name: res.name, capacity: res.capacity,
+                                        current_supply: res.current_supply }
     }
 
     respond_with RGeo::GeoJSON.encode factory.feature_collection(features)
