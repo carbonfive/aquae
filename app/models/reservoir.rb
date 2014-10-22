63728070e7819f7bace0c3f41d87d52eedaf68f4
Christian Nelson <christian@carbonfive.com>
2014-10-21 19:22:05 -0700

    Rake db:import:reservoirs loads data from the CDEC.
    - db:import:reservoirs task screen scrapes and populates reservoirs (idempotently).
    - current supply now be empty if the data isn't there.
    - current_supply_captured_on notes the date of the measurement.

--- app/models/reservoir.rb
+++ app/models/reservoir.rb
@@ -3,11 +3,11 @@ class Reservoir < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
   validates :capacity, presence: true, numericality: { greater_than: 0 }
-  validates :current_supply, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
-  belongs_to :water_system
+  validates :current_supply, numericality: { greater_than_or_equal_to: 0 }
 
   set_rgeo_factory_for_column(:latlon,
                               RGeo::Geographic.spherical_factory(:srid => 4326))
+
+  belongs_to :water_system
 
 end
